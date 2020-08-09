aa09aba5cc3ffe40bcff896edb653d440c4b7483
omeryttnc <omeryttnc@gmail.com>
2020-08-09 18:11:35 +0300

    second commit from local master

--- firstClass.java
+++ firstClass.java
@@ -3,5 +3,6 @@ package package2;
 public class firstClass {
     public static void main(String[] args) {
         System.out.println("first change from local");
+        System.out.println("this is second commit from local master");
     }
 }
