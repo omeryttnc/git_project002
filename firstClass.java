8dba09956c38d29c23b896f58951cd9b6af4b86a
omeryttnc <omeryttnc@gmail.com>
2020-08-09 18:18:23 +0300

    third commit

--- firstClass.java
+++ firstClass.java
@@ -4,5 +4,7 @@ public class firstClass {
     public static void main(String[] args) {
         System.out.println("first change from local");
         System.out.println("this is second commit from local master");
+        System.out.println("this is third commit from local master");
+
     }
 }
